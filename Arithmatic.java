74b13ca94df49576da4605b1e6131060d716e97f
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-05-28 23:47:20 -0700

    to know about arithmatic operators

---
+++ Arithmatic.java
@@ -0,0 +1,10 @@
+public class Arithmatic {
+	public static void main(String s[]){
+	int a=10,b=20;
+	System.out.println("Addition is "+(byte)a+b);
+	System.out.println("\nSubraction is "+a-b);
+	System.out.println("\nMultiplication is "+a*b);
+	System.out.println("\nDivision is "+a/b);
+	System.out.println("\nModulo is "+a%b);
+}
+}
